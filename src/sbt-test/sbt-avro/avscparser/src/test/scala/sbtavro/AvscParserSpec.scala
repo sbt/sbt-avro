b43acad54cf2cf7c4ce9fd94209206215d706ed7
Michel Davit <michel.davit@gmail.com>
2020-07-31 11:48:57 +0200

    Remove trace leftover

--- src/sbt-test/sbt-avro/avscparser/src/test/scala/sbtavro/AvscParserSpec.scala
+++ src/sbt-test/sbt-avro/avscparser/src/test/scala/sbtavro/AvscParserSpec.scala
@@ -11,7 +11,6 @@ import com.cavorite.test.avscparser.A
 class AvscParserSpec extends Specification {
 
   "A should have artifact property" >> {
-    println(A.getClassSchema().getProp("com.cavorite.sbt-avro.artifact"))
     A.getClassSchema().getProp("com.cavorite.sbt-avro.artifact") == "avscparser-test:avscparser-test:0.1.0-SNAPSHOT"
   }
 
