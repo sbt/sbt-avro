974c82cfa658d08c3ef3efc11cb0c5093114829a
Mārtiņš Kalvāns <martins.kalvans@gmail.com>
2020-08-05 12:19:28 +0200

    Use scala collection as argument type for DefaultSchemaParserBuilder

--- src/main/scala/sbtavro/DefaultSchemaParserBuilder.scala
+++ src/main/scala/sbtavro/DefaultSchemaParserBuilder.scala
@@ -1,18 +1,17 @@
 package sbtavro
 
-import java.util
-
 import com.spotify.avro.mojo.SchemaParserBuilder
 import org.apache.avro.Schema
+import scala.collection.JavaConverters._
 
-case class DefaultSchemaParserBuilder(types: util.Map[String, Schema],
+case class DefaultSchemaParserBuilder(types: Iterable[Schema],
                                       validate: Boolean,
                                       validateDefaults: Boolean)
     extends SchemaParserBuilder {
 
   override def build(): Schema.Parser = {
     val parser = new Schema.Parser
-    parser.addTypes(types)
+    parser.addTypes(types.map(el => el.getFullName() -> el).toMap.asJava)
     parser.setValidate(validate)
     parser.setValidateDefaults(validateDefaults)
     parser
@@ -26,7 +25,7 @@ object DefaultSchemaParserBuilder {
 
   def template(template: Schema.Parser): DefaultSchemaParserBuilder = {
     DefaultSchemaParserBuilder(
-      template.getTypes,
+      template.getTypes.values().asScala,
       template.getValidate,
       template.getValidateDefaults
     )
