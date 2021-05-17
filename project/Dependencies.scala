98e190abc1a0595eee44aa01950f6206c3067382
Neville Li <neville@spotify.com>
2021-05-17 19:07:11 -0400

    switch to GitHub Actions (#70)
    * switch to GitHub Actions
    
    * Integrate with sbt-github-actions plugin
    
    Co-authored-by: Michel Davit <michel.davit@gmail.com>

---
+++ project/Dependencies.scala
@@ -0,0 +1,17 @@
+import sbt._
+
+object Dependencies {
+
+  object Versions {
+    val Avro = "1.10.2"
+    val Specs2 = "4.11.0"
+  }
+
+  object Provided {
+    val AvroCompiler =  "org.apache.avro" % "avro-compiler" % Versions.Avro % "provided"
+  }
+
+  object Test {
+    val Spec2 = "org.specs2" %% "specs2-core" % Versions.Specs2 % "test"
+  }
+}
