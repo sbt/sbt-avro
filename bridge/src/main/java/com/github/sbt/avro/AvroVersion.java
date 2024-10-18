a276e3cce24b3aa059a9a45763ade3bd03ab12a9
Michel Davit <michel@davit.fr>
2024-10-18 12:06:39 +0200

    Rework optional getters option

--- bridge/src/main/java/com/github/sbt/avro/AvroVersion.java
+++ bridge/src/main/java/com/github/sbt/avro/AvroVersion.java
@@ -2,16 +2,47 @@ package com.github.sbt.avro;
 
 import org.apache.avro.Schema;
 
-public class AvroVersion {
+public class AvroVersion implements Comparable<AvroVersion> {
 
-    private static String[] AVRO_VERSION_PARTS =
-            Schema.class.getPackage().getImplementationVersion().split("\\.", 3);
+    private final int major;
+    private final int minor;
+    private final int patch;
 
-    static int getMajor() {
-        return Integer.parseInt(AVRO_VERSION_PARTS[0]);
+    public AvroVersion(int major, int minor, int patch) {
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
     }
 
-    static int getMinor() {
-        return Integer.parseInt(AVRO_VERSION_PARTS[1]);
+    int getMajor() {
+        return major;
+    }
+
+    int getMinor() {
+        return minor;
+    }
+
+    int getPatch() {
+        return patch;
+    }
+
+    static AvroVersion getRuntimeVersion() {
+        String[] parts = Schema.class.getPackage().getImplementationVersion().split("\\.", 3);
+        int major = Integer.parseInt(parts[0]);
+        int minor = Integer.parseInt(parts[1]);
+        int patch = Integer.parseInt(parts[2]);
+        return new AvroVersion(major, minor, patch);
+    }
+
+
+    @Override
+    public int compareTo(AvroVersion o) {
+        if (major != o.major) {
+            return major - o.major;
+        } else if (minor != o.minor) {
+            return minor - o.minor;
+        } else {
+            return patch - o.patch;
+        }
     }
 }
